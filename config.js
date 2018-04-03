01dee03d32a38a1c883ed347f2cca2b2880ad18d
Anatas <anatasmloz@foxmail.com>
2018-04-03 20:44:47 +0800

    Init

---
+++ config.js
@@ -0,0 +1,38 @@
+config = {};
+config.core = {
+    user :  {
+         username : "13401655897",
+         password : "HHUCrawlQiChaCha"
+    },
+    url : {
+        home : "https://www.qichacha.com",
+        login : "https://www.qichacha.com/user_login",
+        search : "https://www.qichacha.com/search?key="
+    },
+    selector : {
+        username : '#nameNormal',
+        password : '#pwdNormal',
+        search_page_num : 10,
+        search_res_num : 10,
+        is_search_res : '#company-top > div > div.content > div.row.title',
+        nextPage : '#ajaxpage'
+    },
+    xpath : {
+        username : '//*[@id="nameNormal"]',
+        password : '//*[@id="pwdNormal"]'
+    },
+    time : {
+      pageWait : 3000,
+      infoWait : 100,
+      forInvesNextPage : 2000,
+    }
+};
+config.SearchSelector = function(index)
+{
+  return '#searchlist > table > tbody > tr:nth-child(' + index.toString() + ') > td:nth-child(2) > a';
+};
+config.SearchURL = function(keyword, index)
+{
+  return config.core.url.search + keyword.toString() + '#p:' + index.toString() + '&';
+};
+module.exports = config;
---
+++ mysql.js
@@ -0,0 +1,81 @@
+mysql = {};
+
+mysql.core = {
+  user :  {
+       username : 'eye',
+       password : 'Anatas'
+  },
+  localhost : '115.159.39.220',
+  database : 'Eye'
+};
+
+mysql.companySel = {
+  Name : '#fapiao-title > div.panel-body.text-center > div.m-t-md.TaxView > p:nth-child(1) > span',
+  Logo : '#company-top > div > div.logo > div.imgkuang > img',
+  Phone  : '#company-top > div > div.content > div:nth-child(2) > span.cvlu > span',
+  Email : '#company-top > div > div.content > div:nth-child(3) > span:nth-child(2) > a',
+  Website : '#company-top > div > div.content > div:nth-child(3) > span:nth-child(4) > a',
+  BossName : '#Cominfo > table:nth-child(3) > tbody > tr:nth-child(2) > td.ma_left > div > div.clearfix > div:nth-child(2) > a.bname',
+  RegisterMoney : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(1) > td:nth-child(2)',
+  ActualMoney : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(1) > td:nth-child(4)',
+  State : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(2)',
+  RegisterDate : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(4)',
+  RegisterID : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(3) > td:nth-child(2)',
+  OrganizationID : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(3) > td:nth-child(4)',
+  PayPersonID : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(4) > td:nth-child(2)',
+  UniqueSocietyID : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(4) > td:nth-child(4)',
+  CompanyType : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(5) > td:nth-child(2)',
+  Profession : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(5) > td:nth-child(4)',
+  ConfirmDate : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(6) > td:nth-child(2)',
+  RegisterOrganiztion : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(6) > td:nth-child(4)',
+  Region : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(7) > td:nth-child(2)',
+  EnglishName : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(7) > td:nth-child(4)',
+  EverName : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(8) > td:nth-child(2) > span',
+  SellType : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(8) > td:nth-child(4)',
+  Faculty : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(9) > td:nth-child(2)',
+  SellTime : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(9) > td:nth-child(4)',
+  Place : '#company-top > div > div.content > div:nth-child(4) > span.cvlu > a:nth-child(1)',
+  MainProduct : '#Cominfo > table:nth-child(4) > tbody > tr:nth-child(11) > td:nth-child(2)',
+}
+
+mysql.num = {
+  invesSel : "#Sockinfo > div > span.tbadge",
+  manSel :　"#Mainmember > div > span.tbadge",
+  ForInveSel : "#touzilist > div.tcaption > span.tbadge",
+  ForInveNum : 10
+}
+mysql.forInveNextPage = function (index) {
+  if(index > 2)
+  {
+    index = index + 1;
+  }
+  return "#touzilist > div:nth-child(3) > nav > ul > li:nth-child(" + index.toString() + ") > a";
+}
+mysql.getForInvesNum = function(index)
+{
+  index = index + 1;
+  res = {};
+  res.CompanyName = "#touzilist > table > tbody > tr:nth-child(" + index.toString() + ") > td:nth-child(1) > a";
+  res.InvestMoney = "#touzilist > table > tbody > tr:nth-child(" + index.toString() + ") > td:nth-child(3)",
+  res.InvestRate = "#touzilist > table > tbody > tr:nth-child(" + index.toString() + ") > td:nth-child(4)";
+  res.SellDate = "#touzilist > table > tbody > tr:nth-child(" + index.toString() + ") > td:nth-child(5)";
+  return res;
+}
+mysql.getInvesSel = function (index) {
+  index = index + 1;
+  res = {};
+  res.InvestorName = "#Sockinfo > table > tbody > tr:nth-child(" + index.toString() + ") > td:nth-child(2) > a";
+  res.InvestRate = "#Sockinfo > table > tbody > tr:nth-child(" + index.toString() +  ") > td:nth-child(3)";
+  res.InvestMoney = "#Sockinfo > table > tbody > tr:nth-child(" + index.toString() +  ") > td:nth-child(4)";
+  res.SellDate = "#Sockinfo > table > tbody > tr:nth-child(" + index.toString() +  ") > td:nth-child(5)";
+  res.Type = "#Sockinfo > table > tbody > tr:nth-child(" + index.toString() +  ") > td:nth-child(6)";
+  return res;
+}
+
+mysql.getManSel = function (index) {
+  index = index + 1;
+  res.Name = "#Mainmember > table > tbody > tr:nth-child(" + index.toString() + ") > td:nth-child(2) > a:nth-child(1)";
+  res.Job = "#Mainmember > table > tbody > tr:nth-child(" + index.toString() + ") > td.text-center";
+  return res;
+}
+module.exports = mysql;
